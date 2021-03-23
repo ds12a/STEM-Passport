7022978b246642866a944a26bed30916cdbd2b1a
dshen12 <81164854+dshen12@users.noreply.github.com>
2021-03-23 11:28:20 -0400

    Update vendors.js

--- app/vendors.js
+++ app/vendors.js
@@ -89,35 +89,14 @@ function setCookie(cname, cvalue) {
   var expires = "expires="+ d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-function getCookie( name ) {
-    var dc,
-        prefix,
-        begin,
-        end;
-    
-    dc = document.cookie;
-    prefix = name + "=";
-    begin = dc.indexOf("; " + prefix);
-    end = dc.length; // default to end of the string
-
-    // found, and not in first position
-    if (begin !== -1) {
-        // exclude the "; "
-        begin += 2;
-    } else {
-        //see if cookie is in first position
-        begin = dc.indexOf(prefix);
-        // not found at all or found as a portion of another cookie name
-        if (begin === -1 || begin !== 0 ) return null;
-    } 
-
-    // if we find a ";" somewhere after the prefix position then "end" is that position,
-    // otherwise it defaults to the end of the string
-    if (dc.indexOf(";", begin) !== -1) {
-        end = dc.indexOf(";", begin);
+function getCookie(name){
+    var pattern = RegExp(name + "=.[^;]*")
+    var matched = document.cookie.match(pattern)
+    if(matched){
+        var cookie = matched[0].split('=')
+        return cookie[1]
     }
-
-    return decodeURI(dc.substring(begin + prefix.length, end) ).replace(/\"/g, ''); 
+    return false
 }
 function getUserData(){
   firebase.auth().onAuthStateChanged(function(user) {
@@ -156,7 +135,7 @@ function makeCard(vendor, visited) {
 function getCards() {
   var s = "";
   var v = getCookie('placesVisited');
-  if(v == null) {
+  if(!v) {
     getUserData();
     v = getCookie('placesVisited');
   }
