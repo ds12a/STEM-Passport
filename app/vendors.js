bf77ea7854134f088d0d49616f104c53296635f8
dshen12 <81164854+dshen12@users.noreply.github.com>
2021-03-23 11:15:33 -0400

    Update vendors.js

--- app/vendors.js
+++ app/vendors.js
@@ -1,7 +1,12 @@
 // REMOVE BEFORE DEPLOY
-window.onerror = function(msg, url, linenumber) {
-    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
-    return true;
+window.addEventListener("error", handleError, true);
+
+function handleError(evt) {
+    if (evt.message) { // Chrome sometimes provides this
+      alert("error: "+evt.message +" at linenumber: "+evt.lineno+" of file: "+evt.filename);
+    } else {
+      alert("error: "+evt.type+" from element: "+(evt.srcElement || evt.target));
+    }
 }
 // REMOVE BEFORE DEPLOY
 var vendors = [
