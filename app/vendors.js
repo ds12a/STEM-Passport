96bc80977b744b01fc2c039c4ac033e49edfba1f
dshen12 <81164854+dshen12@users.noreply.github.com>
2021-03-25 14:17:26 -0400

    Update vendors.js

--- app/vendors.js
+++ app/vendors.js
@@ -111,6 +111,7 @@ function toggle(id){alert(id);
     Cookies.set('placesVisited', c.join("|"), {path: '' });
     Cookies.set('timestamps', c2.join("|"), {path: '' });
                     alert("Cookies updated");
+    document.getElementById('content').outerHTML = getCards();
 }
 function getUserData(){
   firebase.auth().onAuthStateChanged(function(user) {
